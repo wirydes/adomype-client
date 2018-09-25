54e799a10a3baa05e604f74c7d264ea674055662
Aurio Alejandro Robles Osuna <alejandro.ro94@gmail.com>
2018-09-25 00:29:29 -0700

    create a base component

---
+++ src/app/shared/base/base.component.ts
@@ -0,0 +1,20 @@
+import { OnInit, OnDestroy } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+@Injectable()
+export class BaseComponent implements OnInit, OnDestroy {
+
+  onSubscribe: Subscription[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy(): void {
+    this.onSubscribe.forEach(item => {
+      item.unsubscribe();
+    });
+  }
+
+}
