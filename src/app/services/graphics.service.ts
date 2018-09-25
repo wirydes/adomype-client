918da4be5014058787949a35285ce09564b0549a
Aurio Alejandro Robles Osuna <alejandro.ro94@gmail.com>
2018-09-25 00:30:03 -0700

    create get graphics service

---
+++ src/app/services/graphics.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { SurveyDropDownModel } from '../models/survey.dropdown.model';
+import { ChartsConfigModel } from '../models/charts.config.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GraphicsService {
+  private url = 'localhost:3000/api/adomype';
+
+  constructor(private http: HttpClient) { }
+
+  getDropDownInfo(): Observable<SurveyDropDownModel[]> {
+    return this.http.get<SurveyDropDownModel[]>(this.url + '/dropdown/info');
+  }
+
+  getSurveyChartInfo(id: number): Observable<ChartsConfigModel> {
+    return this.http.get<ChartsConfigModel>(this.url + '/chart/config/' + id);
+  }
+}
