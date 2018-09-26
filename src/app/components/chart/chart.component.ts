26f03b1058fd2269db772eb54c7ea3a62ce219d8
Aurio Alejandro Robles Osuna <alejandro.ro94@gmail.com>
2018-09-26 01:11:27 -0700

    add some logic to the chart component

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent implements OnInit {
+export class ChartComponent implements OnInit, OnChanges {
+  @Input() chartData: any;
+  @Input() chartType: string;
 
+  chart = [];
   constructor() { }
 
   ngOnInit() {
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (!!changes.chartData.currentValue) {
+    // this.chart = new Chart('canvas', {
+    //   type: 'line',
+    //   data: {
+    //     labels: weatherDates,
+    //     datasets: [
+    //       {
+    //         data: temp_max,
+    //         borderColor: "#3cba9f",
+    //         fill: false
+    //       },
+    //       {
+    //         data: temp_min,
+    //         borderColor: "#ffcc00",
+    //         fill: false
+    //       },
+    //     ]
+    //   },
+    //   options: {
+    //     legend: {
+    //       display: false
+    //     },
+    //     scales: {
+    //       xAxes: [{
+    //         display: true
+    //       }],
+    //       yAxes: [{
+    //         display: true
+    //       }],
+    //     }
+    //   }
+    // });
+    }
+  }
+
 
 }
