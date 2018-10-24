b55c8b072bd75c987d378094c6d54c21a262cef8
Aurio Alejandro Robles Osuna <alejandro.ro94@gmail.com>
2018-10-23 23:50:51 -0700

    update recomendation component

--- src/app/components/recommendation/recommendation.component.ts
+++ src/app/components/recommendation/recommendation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MatureProfileChartModel } from 'src/app/models/mature.profile.chart.model';
+import { ChartService } from 'src/app/services/chart.service';
 
 @Component({
   selector: 'app-recommendation',
@@ -8,9 +9,28 @@ import { MatureProfileChartModel } from 'src/app/models/mature.profile.chart.mod
 })
 export class RecommendationComponent implements OnInit {
   @Input() chartData: MatureProfileChartModel;
-  constructor() { }
+  constructor(private chartService: ChartService) { }
 
   ngOnInit() {
   }
+
+  checkPart(section: number, part: number): boolean {
+    const sectionTitle = 'section';
+    const sectionName = sectionTitle + section;
+    const partValue = this.chartData[sectionName].parts.find((element, index) => {
+      return (index + 1) === part;
+    });
+
+    if (partValue !== undefined) {
+      const percentage = this.chartService.getMaturePercentage(parseFloat(partValue));
+      if (percentage === 'Artesanal' || percentage === 'Incipiente') {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+
 
 }
