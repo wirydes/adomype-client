2fd6ad9e8926e6b8ca8a81273887cecc7f3e3f61
Aurio Alejandro Robles Osuna <alejandro.ro94@gmail.com>
2018-10-26 22:40:15 -0700

    update routes to use just 1 route the base

--- src/app/routing.ts
+++ src/app/routing.ts
@@ -1,13 +1,8 @@
-import { HomeComponent } from './components/home/home.component';
 import { StudentHomeComponent } from './components/student-home/student-home.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [{
     path: 'home',
-    component: StudentHomeComponent
-},
-{
-    path: '',
     component: StudentHomeComponent
 }
 ];
