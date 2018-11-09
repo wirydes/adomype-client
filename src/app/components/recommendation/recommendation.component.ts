c74375f7ebbb6a83e6749a9c1c4ca3fd23bf0db9
Aurio Alejandro Robles Osuna <alejandro.ro94@gmail.com>
2018-11-09 00:20:47 -0700

    update recomendation component

--- src/app/components/recommendation/recommendation.component.ts
+++ src/app/components/recommendation/recommendation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MatureProfileChartModel } from 'src/app/models/mature.profile.chart.model';
 import { ChartService } from 'src/app/services/chart.service';
+import { SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-recommendation',
@@ -9,9 +10,31 @@ import { ChartService } from 'src/app/services/chart.service';
 })
 export class RecommendationComponent implements OnInit {
   @Input() chartData: MatureProfileChartModel;
+  sections: number[] = [];
+  recomendations: {section: number, part: number, color: SafeStyle}[] = [];
   constructor(private chartService: ChartService) { }
 
   ngOnInit() {
+    this.checkPart(1, 1);
+    this.checkPart(1, 2);
+    this.checkPart(1, 3);
+    this.checkPart(2, 1);
+    this.checkPart(2, 2);
+    this.checkPart(2, 3);
+    this.checkPart(3, 1);
+    this.checkPart(3, 2);
+    this.checkPart(3, 3);
+    this.checkPart(4, 1);
+    this.checkPart(4, 2);
+    this.checkPart(5, 1);
+    this.checkPart(5, 2);
+    this.checkPart(6, 1);
+    this.checkPart(6, 2);
+    this.checkPart(7, 1);
+    this.checkPart(7, 2);
+    this.checkPart(7, 3);
+    this.checkPart(7, 4);
+
   }
 
   checkPart(section: number, part: number): boolean {
@@ -22,14 +45,41 @@ export class RecommendationComponent implements OnInit {
     });
 
     if (!!partValue) {
-      const percentage = this.chartService.getMatureLv(parseInt(partValue.value, 10));
-      if (percentage === 'Artesanal' || percentage === 'Incipiente') {
+      const value = parseInt(partValue.value, 10);
+      const matureLv = this.chartService.getMatureLv(value);
+      if (matureLv === 'Artesanal' || matureLv === 'Incipiente') {
+        this.sections.push(section);
+        this.recomendations.push({section, part, color: this.chartService.getColor(value + '')});
         return true;
       }
     }
 
     return false;
   }
+
+  checkProcessTitle(section) {
+    return this.sections.some((item) => {
+      return item === section;
+    });
+  }
+
+  checkRecomendation(section: number, part: number) {
+    return this.recomendations.some((item) => {
+      return item.section === section && item.part === part;
+    });
+  }
+
+  getColor(section: number, part: number) {
+    const selected = this.recomendations.find((item) => {
+      return item.section === section && item.part === part;
+    });
+
+    if (selected) {
+      return selected.color;
+    }
+  }
+
+
 
 
 
